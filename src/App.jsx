2dad9b576fc3f79872daebfd1e7e995f074d20a1
Richard <richardyun03@gmail.com>
2026-05-20 12:59:55 -0400

    Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 
 
 export default function App() {
@@ -33,6 +34,7 @@ export default function App() {
         </main>
         <Footer />
       </div>
+      <Analytics />
     </div>
   );
 }
